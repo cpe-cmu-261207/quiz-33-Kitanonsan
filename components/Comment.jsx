0eda81de02d5463058693b9e8a88b1740da7f2a1
Kitanonsan <Kitanon_san@cmu.ac.th>
2022-09-15 14:25:18 +0700

    push

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -4,10 +4,43 @@ import Reply from "./Reply";
 export default function Comment(props) {
   return (
     <div>
-      {/* Entire Comment div */}
-      <div></div>
+      <div>
+        <div className="d-flex gap-2 my-2">
+          <img
+            src={props.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {props.username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{props.commentText}</span>
+            {props.likeNum > 0 && (
+              <div className="d-flex align-items-center gap-1">
+                <img src="/like.svg" width={20}></img>
+                <span style={{ color: "#B0B3B8" }}>{props.likeNum} คน</span>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
 
-      {/*render Reply here... */}
+      {props.replies.map((x, i) => (
+        <Reply
+          username={x.username}
+          userImagePath={x.userImagePath}
+          replyText={x.replyText}
+          likeNum={x.likeNum}
+          key={i}
+        />
+      ))}
     </div>
   );
 }
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/NewJeans.webp"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5 text-white">
+          Kitanon Santabut 640610619
+        </span>
+      </div>
+      <span className="text-white">NewJeans น่ารักจังเลยคร้าบบบ ❤️🥰💞</span>
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span style={{ color: "#B0B3B8" }}>300 คน</span>
+      </div>
+      <hr className="m-0 border" />
+    </div>
+  );
 }
